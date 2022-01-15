e3c166847866d0358a471d07a81dc29a4f581c58
lupyuen <luppy@appkaki.com>
2022-01-15 08:35:49 +0800

    Update script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,42 +1,39 @@
 #!/usr/bin/env bash
-## Test GPIO Output on Linux SBC
-## Based on https://www.ics.com/blog/gpio-programming-using-sysfs-interface
+## Flash and test BL602 with GPIO Output on Linux SBC.
+## (1) Before Flashing BL602: Set GPIO 8 to High and toggle the Reset Pin
+## (2) Flash BL602 over USB UART with blflash
+## (3) After Flashing BL602: Set GPIO 8 to Low and toggle the Reset Pin
+## (4) Capture the BL602 output over USB UART
 ## Pins to be connected:
 ## | SBC    | BL602    | Function
 ## | -------|----------|----------
 ## | GPIO 2 | GPIO 8   | Flashing Mode
 ## | GPIO 3 | GPIO RST | Reset
+## Based on https://www.ics.com/blog/gpio-programming-using-sysfs-interface
 
 set -e  ##  Exit when any command fails
 set -x  ##  Echo commands
 
-## Show enabled GPIOs
-ls /sys/class/gpio/
-
 ## Enable GPIO 2 if not enabled
 if [ ! -d /sys/class/gpio/gpio2 ]; then
     echo 2 >/sys/class/gpio/export
-    ls /sys/class/gpio/
 fi
 
 ## Set GPIO 2 as output
 echo out >/sys/class/gpio/gpio2/direction
 
+## Set GPIO 2 to High (BL602 Flashing Mode)
+echo 1 >/sys/class/gpio/gpio2/value
+
+## TODO: Toggle GPIO 3 (Reset BL602) and flash BL602 over USB UART with blflash
+
 ## Wait a while
 sleep 5
 
-## Set GPIO 2 to Low
+## Set GPIO 2 to Low (BL602 Non-Flashing Mode)
 echo 0 >/sys/class/gpio/gpio2/value
 
-## Show GPIO 2 status
-cat /sys/class/gpio/gpio2/value
-
-## Set GPIO 2 to High
-echo 1 >/sys/class/gpio/gpio2/value
-
-## Show GPIO 2 status
-cat /sys/class/gpio/gpio2/value
+## TODO: Toggle GPIO 3 (Reset BL602) and capture the BL602 output over USB UART
 
 ## Disable GPIO 2
 echo 2 >/sys/class/gpio/unexport
-ls /sys/class/gpio
