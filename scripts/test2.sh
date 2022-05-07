615ae4b7fb514825ab4aa081d438ff8e941090bc
Lee Lup Yuen <luppy@appkaki.com>
2022-05-07 18:10:03 +0800

    Fixing BL602

--- scripts/test2.sh
+++ scripts/test2.sh
@@ -30,13 +30,13 @@ fi
 source $HOME/.cargo/env
 
 set +x  ##  Disable echo
-echo "----- Download the latest $BUILD_PREFIX NuttX build for $BUILD_DATE"
-set -x  ##  Enable echo
-wget -q https://github.com/lupyuen/incubator-nuttx/releases/download/$BUILD_PREFIX-$BUILD_DATE/nuttx.zip -O /tmp/nuttx.zip
-pushd /tmp
-unzip -o nuttx.zip
-popd
-set +x  ##  Disable echo
+# echo "----- Download the latest $BUILD_PREFIX NuttX build for $BUILD_DATE"
+# set -x  ##  Enable echo
+# wget -q https://github.com/lupyuen/incubator-nuttx/releases/download/$BUILD_PREFIX-$BUILD_DATE/nuttx.zip -O /tmp/nuttx.zip
+# pushd /tmp
+# unzip -o nuttx.zip
+# popd
+# set +x  ##  Disable echo
 
 echo "----- Enable GPIO 2 and 3"
 if [ ! -d /sys/class/gpio/gpio2 ]; then
@@ -50,25 +50,25 @@ echo "----- Set GPIO 2 and 3 as output"
 echo out >/sys/class/gpio/gpio2/direction
 echo out >/sys/class/gpio/gpio3/direction
 
-echo "----- Set GPIO 2 to High (BL602 Flashing Mode)"
-echo 1 >/sys/class/gpio/gpio2/value ; sleep 1
+# echo "----- Set GPIO 2 to High (BL602 Flashing Mode)"
+# echo 1 >/sys/class/gpio/gpio2/value ; sleep 1
 
-echo "----- Toggle GPIO 3 High-Low-High (Reset BL602)"
-echo 1 >/sys/class/gpio/gpio3/value ; sleep 1
-echo 0 >/sys/class/gpio/gpio3/value ; sleep 1
-echo 1 >/sys/class/gpio/gpio3/value ; sleep 1
+# echo "----- Toggle GPIO 3 High-Low-High (Reset BL602)"
+# echo 1 >/sys/class/gpio/gpio3/value ; sleep 1
+# echo 0 >/sys/class/gpio/gpio3/value ; sleep 1
+# echo 1 >/sys/class/gpio/gpio3/value ; sleep 1
 
-echo "----- Toggle GPIO 3 High-Low-High (Reset BL602 again)"
-echo 1 >/sys/class/gpio/gpio3/value ; sleep 1
-echo 0 >/sys/class/gpio/gpio3/value ; sleep 1
-echo 1 >/sys/class/gpio/gpio3/value ; sleep 1
+# echo "----- Toggle GPIO 3 High-Low-High (Reset BL602 again)"
+# echo 1 >/sys/class/gpio/gpio3/value ; sleep 1
+# echo 0 >/sys/class/gpio/gpio3/value ; sleep 1
+# echo 1 >/sys/class/gpio/gpio3/value ; sleep 1
 
-echo "----- BL602 is now in Flashing Mode"
-echo "----- Flash BL602 over USB UART with blflash"
-set -x  ##  Enable echo
-blflash flash /tmp/nuttx.bin --port $USB_DEVICE
-set +x  ##  Disable echo
-sleep 1
+# echo "----- BL602 is now in Flashing Mode"
+# echo "----- Flash BL602 over USB UART with blflash"
+# set -x  ##  Enable echo
+# blflash flash /tmp/nuttx.bin --port $USB_DEVICE
+# set +x  ##  Disable echo
+# sleep 1
 
 echo "----- Set GPIO 2 to Low (BL602 Normal Mode)"
 echo 0 >/sys/class/gpio/gpio2/value ; sleep 1
@@ -105,10 +105,11 @@ if [ "$match" == "" ]; then
     ##  If BL602 has not crashed, send the test command to BL602
     echo "uname -a" >$USB_DEVICE ; sleep 1
     echo "ls /dev" >$USB_DEVICE ; sleep 1
-    
-    echo ; echo "----- Send command to BL602: spi_test2" ; sleep 2
+    echo "spi_test2" >$USB_DEVICE ; sleep 5
+
+    echo ; echo "----- Send command to BL602: lorawan_test" ; sleep 2
     echo "" >$USB_DEVICE
-    echo "spi_test2" >$USB_DEVICE
+    echo "lorawan_test" >$USB_DEVICE
 
     ##  Wait a while for the test command to run
     sleep 30
