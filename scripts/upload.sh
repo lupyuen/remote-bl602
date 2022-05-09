8bacefb4ff162ea12a9b9ca0dec3b5eeb8e3436a
Lee Lup Yuen <luppy@appkaki.com>
2022-05-09 14:36:28 +0800

    Test upload

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,30 +1,25 @@
 #!/usr/bin/env bash
+## Upload Test Log to GitHub Release Notes. Assumes the following files are present...
+## /tmp/release.log: Test Log
+## /tmp/release.tag: Release Tag
+
 set -e  ##  Exit when any command fails
 set -x  ##  Echo commands
 
-cd ~/remote-bl602
-git pull
-
-##  Set the USB Device and Build Name
-export USB_DEVICE=/dev/ttyUSB0
-
-##  Run the script for Auto Flash and Test
-rm -f /tmp/release.log
-script -c ~/remote-bl602/scripts/test.sh /tmp/release.log
-
-##  Clean up Test Log
 rm -f /tmp/release2.log
+
+##  Show the status
+grep "^===== " /tmp/release.log >>/tmp/release2.log
+
+##  Enquote the log
 echo '```text' >>/tmp/release2.log
 cat /tmp/release.log >>/tmp/release2.log
 echo '```' >>/tmp/release2.log
+
 cp /tmp/release2.log /tmp/release.log
 
 ##  Upload the Test Log to the Release Notes
 gh release edit \
-    upstream-2022-05-09 \
+    `cat /tmp/release.tag` \
     --notes-file /tmp/release.log \
     --repo lupyuen/incubator-nuttx
-
-##  Shutdown
-read -p "Press Enter to shutdown"
-sudo shutdown now
